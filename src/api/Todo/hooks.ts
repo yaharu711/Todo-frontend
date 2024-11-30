890084fa8d63c948b7d2171c9d70242ecc1956be
yaharu711 <yah7yah707@gmail.com>
2024-11-30 11:06:13 +0900

    add loading

--- src/api/Todo/hooks.ts
+++ src/api/Todo/hooks.ts
@@ -1,10 +1,10 @@
-import { useQuery } from "@tanstack/react-query";
+import { useSuspenseQuery } from "@tanstack/react-query";
 import TodoApi from "./functions";
 import { useApiErrorHandles } from "../common/ErrorHanleHook";
 
 export const useGetHelloMessage = () => {
   const errorHandles = useApiErrorHandles();
-  return useQuery({
+  return useSuspenseQuery({
     queryKey: ["hello-message"],
     queryFn: () => TodoApi.getHelloMessage(errorHandles),
   });
--- src/components/PrivateLayout.tsx
+++ src/components/PrivateLayout.tsx
@@ -1,22 +1,26 @@
 import { Outlet } from "react-router-dom";
 import { useGetHelloMessage } from "../api/Todo/hooks";
+import { Suspense } from "react";
+import FadeLoaderOverlapedAll from "./FadeLoaderOverlapedAll";
 
 const PrivateLayout = () => {
-  const { data } = useGetHelloMessage();
   // このような処理は上位コンポーネントでSuspenseを使えばOK
   //   if (status === "error" || status === "pending") {
   //     return <span>Loading...</span>;
   //   }
   // 以下の用にしていないと、useGetHelloMessage()の処理が終わる前にreturn <Outlet />に来てしまうので
   // 認証されていないときも最初に画面が表示されてしまうので注意
-  return data && <Outlet />;
+  return (
+    <Suspense fallback={<FadeLoaderOverlapedAll />}>
+      <OutletWithAuthenticated />
+    </Suspense>
+  );
 };
 
-//これをPrivateLayoutでSuspenseで囲えばロード中の挙動をSuspenseで制御できる
-// const Content = () => {
-//   const { data } = useGetHelloMessage();
+const OutletWithAuthenticated = () => {
+  const { data } = useGetHelloMessage();
 
-//   return data ? <Outlet /> : null;
-// };
+  return data ? <Outlet /> : null;
+};
 
 export default PrivateLayout;
