0dd668ab76fbc9b33fa8f250e65fc2633ddc5e6f
yaharu711 <yah7yah707@gmail.com>
2024-12-16 21:34:37 +0900

    add login form

--- src/components/TodoInputForm.tsx
+++ src/components/TodoInputForm.tsx
@@ -1,59 +1,65 @@
-import React, { SetStateAction, useState } from 'react'
-import styles from './TodoInputForm.module.css';
-import Button from './Button';
-import TextInput from './TextInput';
-import type { TodoType } from '../App';
+import React, { SetStateAction, useState } from "react";
+import styles from "./TodoInputForm.module.css";
+import Button from "./Button";
+import TextInput from "./TextInput";
+import type { TodoType } from "../App";
 
 type TodoInputFormProps = {
-    imcompletedTodos: TodoType[],
-    setImcompletedTodos: React.Dispatch<SetStateAction<TodoType[]>>
-}
+  imcompletedTodos: TodoType[];
+  setImcompletedTodos: React.Dispatch<SetStateAction<TodoType[]>>;
+};
 
-const TodoInputForm: React.FC<TodoInputFormProps> = ({imcompletedTodos, setImcompletedTodos}) => {
-    const [inputedTodoName, setInputedTodoName] = useState<string>("");
-    const [inputError, setInputError] = useState<string>('');
+const TodoInputForm: React.FC<TodoInputFormProps> = ({
+  imcompletedTodos,
+  setImcompletedTodos,
+}) => {
+  const [inputedTodoName, setInputedTodoName] = useState<string>("");
+  const [inputError, setInputError] = useState<string>("");
 
-    const createTodo = (): void => {
-        const imcompletedTodoNames = imcompletedTodos.map(imcompletedTodo => imcompletedTodo.name);
-        if (inputedTodoName === "") {
-            setInputError("入力は必須です");
-            return;
-        }
-        if (imcompletedTodoNames.includes(inputedTodoName)) {
-            setInputError('既に同じTODOがあります');
-            return;
-        }
-        const newTodo: TodoType = {
-            name: inputedTodoName,
-            isEditMode: false
-        }
-        setImcompletedTodos([...imcompletedTodos, newTodo]);
-        setInputedTodoName("");
+  const createTodo = (): void => {
+    const imcompletedTodoNames = imcompletedTodos.map(
+      (imcompletedTodo) => imcompletedTodo.name
+    );
+    if (inputedTodoName === "") {
+      setInputError("入力は必須です");
+      return;
     }
-    const createTodoOnKeyDown = (e: React.KeyboardEvent<HTMLInputElement>): void => {
-        if (e.key !== "Enter") return;
-        createTodo();
-        e.preventDefault();
+    if (imcompletedTodoNames.includes(inputedTodoName)) {
+      setInputError("既に同じTODOがあります");
+      return;
     }
-    return (
-        <div className={styles.wrap}>
+    const newTodo: TodoType = {
+      name: inputedTodoName,
+      isEditMode: false,
+    };
+    setImcompletedTodos([...imcompletedTodos, newTodo]);
+    setInputedTodoName("");
+  };
+  const createTodoOnKeyDown = (
+    e: React.KeyboardEvent<HTMLInputElement>
+  ): void => {
+    if (e.key !== "Enter") return;
+    createTodo();
+    e.preventDefault();
+  };
+  return (
+    <div className={styles.wrap}>
+      <TextInput
+        placeholder="TODO名"
+        name="todo_name"
+        value={inputedTodoName}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+          setInputedTodoName(e.target.value);
+          setInputError("");
+        }}
+        onKeyDown={(e) => createTodoOnKeyDown(e)}
+        errorMessage={inputError}
+      />
+      <Button disabled={inputedTodoName === ""} onClick={createTodo}>
+        作成
+      </Button>
+    </div>
+  );
+};
 
-            <TextInput 
-                placeholder="TODO名" 
-                value={inputedTodoName}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                    setInputedTodoName(e.target.value);
-                    setInputError('');
-                }}
-                onKeyDown={(e) => createTodoOnKeyDown(e)}
-                errorMessage={inputError}
-            />
-            <Button
-                disabled={inputedTodoName === ""}
-                onClick={createTodo}
-            >作成</Button>
-        </div>
-  )
-}
-
-export default TodoInputForm
+export default TodoInputForm;
