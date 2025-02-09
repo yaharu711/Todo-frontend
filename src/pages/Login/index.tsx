e54149f233e91b45c83529b73b6dbe8ae8b3d772
yaharu711 <yah7yah707@gmail.com>
2025-02-09 09:39:09 +0900

    add link about login, regist

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import FadeLoaderOverlapedAll from "../../components/FadeLoaderOverlapedAll";
 import LoginForm from "./components/LoginForm/LoginForm";
 import styles from "./index.module.css";
@@ -6,10 +7,17 @@ import useLoginPageViewModel from "./useLoginPageViewModel";
 const LoginPage = () => {
   const { isPendingForCheckLogined } = useLoginPageViewModel();
   return (
-    <div className={styles.container}>
-      <h1>ログインページ</h1>
-      {isPendingForCheckLogined ? <FadeLoaderOverlapedAll /> : <LoginForm />}
-    </div>
+    <>
+      <div className={styles.container}>
+        <h1>ログインページ</h1>
+        {isPendingForCheckLogined ? <FadeLoaderOverlapedAll /> : <LoginForm />}
+      </div>
+      <div className={styles.link_container}>
+        <NavLink to="/regist" color="var(--color-thema)">
+          新規登録ページへ
+        </NavLink>
+      </div>
+    </>
   );
 };
 
