1668dab209080923e12b8988802bb39a8ff33cb9
yaharu711 <yah7yah707@gmail.com>
2025-02-19 16:41:52 +0900

    add memo icon

--- src/pages/Todo/components/ImcompletedTodo/ImcompletedTodo.tsx
+++ src/pages/Todo/components/ImcompletedTodo/ImcompletedTodo.tsx
@@ -10,6 +10,7 @@ import TextInput from "../../../../components/TextInput";
 import { isMobile } from "react-device-detect";
 import Button from "../../../../components/Button";
 import UseImcompletedTodoViewModel from "./useImcompletedTodoViewModel";
+import { MdOutlineStickyNote2 } from "react-icons/md";
 
 type Props = {
   target: ImcompletedTodoType;
@@ -37,15 +38,19 @@ const ImcompletedTodo = ({
     editTodoOnKeyDown,
     editTodoOnBlur,
     onChangeEditMode,
+    hasAdditionalInfo,
   } = UseImcompletedTodoViewModel({ target, updateTodoDetail });
 
   return (
     <li className={styles.li}>
-      <div className={styles.todo_name_wrapp}>
-        <IconButton
-          onClick={() => onChangeEditMode()}
-          children={<CiEdit size={25} style={{ color: "var(--color-icon)" }} />}
-        />
+      <IconButton
+        onClick={() => onChangeEditMode()}
+        children={<CiEdit size={25} style={{ color: "var(--color-icon)" }} />}
+      />
+      <div
+        className={styles.todo_name_wrapp}
+        data-has-additional-info={hasAdditionalInfo}
+      >
         {/* TODO: 編集モードの時スタイルが崩れるから、どうにかする */}
         {isEditMode ? (
           <TextInput
@@ -70,6 +75,17 @@ const ImcompletedTodo = ({
           >
             {target.name}
           </p>
+        )}
+        {hasAdditionalInfo && (
+          <div className={styles.additional_info_wrapper}>
+            <MdOutlineStickyNote2
+              onClick={() => toggleModal(target)}
+              size={15}
+            />
+            <span className={styles.memo} onClick={() => toggleModal(target)}>
+              メモ
+            </span>
+          </div>
         )}
       </div>
       <div className={styles.buttons_wrap} data-is-edit-mode={isEditMode}>
