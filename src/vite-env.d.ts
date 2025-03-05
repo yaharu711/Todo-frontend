defab56c65ee9adf0bb63e1f1dbb0fb01da4cddc
yaharu711 <yah7yah707@gmail.com>
2025-03-05 22:39:27 +0900

    add firebase.js

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,6 +2,30 @@
 interface ImportMetaEnv {
   readonly VITE_API_URL: string;
 }
+interface ImportMetaEnv {
+  readonly VITE_FIREBASE_API_KEY: string;
+}
+interface ImportMetaEnv {
+  readonly VITE_FIREBASE_AUTH_DOMAIN: string;
+}
+interface ImportMetaEnv {
+  readonly VITE_FIREBASE_PROJECT_ID: string;
+}
+interface ImportMetaEnv {
+  readonly VITE_FIREBASE_STORAGE_BUCKET: string;
+}
+interface ImportMetaEnv {
+  readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string;
+}
+interface ImportMetaEnv {
+  readonly VITE_FIREBASE_APP_ID: string;
+}
+interface ImportMetaEnv {
+  readonly VITE_FIREBASE_MESUREMENT_ID: string;
+}
+interface ImportMetaEnv {
+  readonly VITE_VAPID_KEY: string;
+}
 
 interface ImportMeta {
   readonly env: ImportMetaEnv;
