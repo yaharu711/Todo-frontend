7f70ffa60eb7b46fc4c21acbe9547679b21636f0
yaharu711 <yah7yah707@gmail.com>
2025-03-05 21:45:03 +0900

    add https setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    server: {
-      host: '0.0.0.0',
-      port: 5173, // 必要に応じてポート番号を指定
-      watch: { //ホットリロードの設定
-        usePolling: true, // ファイル変更のポーリングを使用
-        interval: 100 // ポーリングの間隔（ミリ秒）
-    }
-  }
-})
+  plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+    port: 5173, // 必要に応じてポート番号を指定`
+    watch: {
+      //ホットリロードの設定
+      usePolling: true, // ファイル変更のポーリングを使用
+      interval: 100, // ポーリングの間隔（ミリ秒）
+    },
+  },
+});
