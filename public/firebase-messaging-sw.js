2962821d2782cd1842a4c6fd768121d8ebe16e93
yaharu711 <yah7yah707@gmail.com>
2025-03-23 10:13:32 +0900

    add notification setting

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -11,16 +11,28 @@ self.addEventListener("push", function (event) {
     const data = event.data.json();
     const title = data?.notification?.title ?? "新しい通知";
     const message = data?.notification?.body ?? "詳細を確認してください";
+    const icon = data?.notification?.icon ?? "";
+    const click_action = data?.notification?.click_action ?? "";
 
     event.waitUntil(
       self.registration.showNotification(title, {
         body: message,
+        icon: icon,
+        data: {
+          url: click_action,
+        },
       })
     );
     console.log("通知処理完了");
   } catch (error) {
     console.error("🚨 プッシュ通知の解析に失敗しました:", error);
   }
+});
+
+// TODO: 通知されたtodoのidを付与して、そのTodoの詳細モーダル表示するようにしたいね
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+  event.waitUntil(clients.openWindow(event.notification.data.url));
 });
 
 // 以下はなくても、getToken()を再実行すると、新しいService Workerが適応されるみたい
