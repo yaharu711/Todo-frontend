d173a57f24b6b818cc293358c8f8455be1185cea
yaharu711 <yah7yah707@gmail.com>
2025-06-09 23:13:00 +0900

    add back button component

--- src/pages/Setting/components/SettingHeader/SettingHeader.tsx
+++ src/pages/Setting/components/SettingHeader/SettingHeader.tsx
@@ -1,28 +1,10 @@
-import { useNavigate } from "react-router-dom";
-import Button from "../../../../components/Button";
 import styles from "./SettingHeader.module.css";
+import BackButton from "../../../../components/BackButton";
 
 const SettingHeader = () => {
-  const navigate = useNavigate();
   return (
     <div className={styles.settings_header}>
-      <Button
-        onClick={() => navigate("/todos")}
-        style={{
-          background: "none",
-          border: "none",
-          color: "white",
-          fontSize: "35px",
-          cursor: "pointer",
-          padding: 0,
-          width: "25px",
-          marginRight: "5px",
-          marginBottom: "5px",
-        }}
-      >
-        &lt;
-      </Button>
-      <h2 className={styles.settings_title}>アプリ設定</h2>
+      <BackButton />
     </div>
   );
 };
