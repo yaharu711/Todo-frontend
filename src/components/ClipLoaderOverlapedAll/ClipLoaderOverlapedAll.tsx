5f940b6051fb127d743ecd01cdc8d28f9183990a
yaharu711 <yah7yah707@gmail.com>
2025-06-24 22:58:40 +0900

    modify var z-index

--- src/components/ClipLoaderOverlapedAll/ClipLoaderOverlapedAll.tsx
+++ src/components/ClipLoaderOverlapedAll/ClipLoaderOverlapedAll.tsx
@@ -3,8 +3,8 @@ import styles from "./ClipLoaderOverlapedAll.module.css";
 
 const ClipLoaderOverlapedAll = () => {
   return (
-    <div className={styles.spinnersOverlay}>
-      <div className={styles.spinnersContent}>
+    <div className={styles.loader_overlay}>
+      <div className={styles.loader}>
         <ClipLoader size={80} color="rgba(255, 255, 255, 0.9)" />
       </div>
     </div>
