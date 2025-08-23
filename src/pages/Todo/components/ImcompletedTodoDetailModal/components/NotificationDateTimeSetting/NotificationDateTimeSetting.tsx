d226673ca58e9ccd139e9559242058bf87ba6f67
yaharu711 <yah7yah707@gmail.com>
2025-08-23 11:16:32 +0900

    feat: separate time selection by hour and minute

--- src/pages/Todo/components/ImcompletedTodoDetailModal/components/NotificationDateTimeSetting/NotificationDateTimeSetting.tsx
+++ src/pages/Todo/components/ImcompletedTodoDetailModal/components/NotificationDateTimeSetting/NotificationDateTimeSetting.tsx
@@ -26,9 +26,11 @@ const NotificationDateTimeSetting = ({
         selected={selectedDateTime}
         onChange={onChangeDateTime}
         customInput={<CustomDateDisplay />}
-        showTimeSelect
+        showTimeInput
+        customTimeInput={
+          <CustomTimeInput minTime={minTime} maxTime={maxTime} />
+        }
         timeFormat="HH:mm"
-        timeIntervals={1} // 分単位での選択
         dateFormat="M月d日 H時m分"
         placeholderText="日時を選択"
         minDate={now} // 過去の日付を選択不可にする
@@ -73,5 +75,97 @@ const CustomDateDisplay = React.forwardRef<HTMLSpanElement, CustomInputProps>(
     );
   }
 );
+
+type CustomTimeInputProps = {
+  date?: Date;
+  onChange?: (date: Date) => void;
+  minTime: Date;
+  maxTime: Date;
+};
+
+const CustomTimeInput = ({
+  date = new Date(),
+  onChange = () => undefined,
+  minTime,
+  maxTime,
+}: CustomTimeInputProps) => {
+  const [hour, setHour] = React.useState(date.getHours());
+  const [minute, setMinute] = React.useState(date.getMinutes());
+
+  React.useEffect(() => {
+    setHour(date.getHours());
+    setMinute(date.getMinutes());
+  }, [date]);
+
+  const hours = React.useMemo(() => {
+    const start = minTime.getHours();
+    const end = maxTime.getHours();
+    return Array.from({ length: end - start + 1 }, (_, i) => start + i);
+  }, [minTime, maxTime]);
+
+  const minutes = React.useMemo(() => {
+    const start =
+      hour === minTime.getHours() ? minTime.getMinutes() : 0;
+    const end = hour === maxTime.getHours() ? maxTime.getMinutes() : 59;
+    return Array.from({ length: end - start + 1 }, (_, i) => start + i);
+  }, [hour, minTime, maxTime]);
+
+  const handleHourChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const newHour = Number(e.target.value);
+    setHour(newHour);
+    const mins =
+      newHour === minTime.getHours()
+        ? minutes.filter((m) => m >= minTime.getMinutes())
+        : newHour === maxTime.getHours()
+        ? minutes.filter((m) => m <= maxTime.getMinutes())
+        : minutes;
+    let newMinute = minute;
+    if (mins.length) {
+      if (newMinute < mins[0]) newMinute = mins[0];
+      if (newMinute > mins[mins.length - 1]) newMinute = mins[mins.length - 1];
+    }
+    setMinute(newMinute);
+    const newDate = new Date(date);
+    newDate.setHours(newHour, newMinute, 0, 0);
+    onChange(newDate);
+  };
+
+  const handleMinuteChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const newMinute = Number(e.target.value);
+    setMinute(newMinute);
+    const newDate = new Date(date);
+    newDate.setHours(hour, newMinute, 0, 0);
+    onChange(newDate);
+  };
+
+  return (
+    <div className={styles.timePicker}>
+      <select
+        className={styles.timeSelect}
+        value={hour}
+        onChange={handleHourChange}
+        size={5}
+      >
+        {hours.map((h) => (
+          <option key={h} value={h}>
+            {String(h).padStart(2, "0")}
+          </option>
+        ))}
+      </select>
+      <select
+        className={styles.timeSelect}
+        value={minute}
+        onChange={handleMinuteChange}
+        size={5}
+      >
+        {minutes.map((m) => (
+          <option key={m} value={m}>
+            {String(m).padStart(2, "0")}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
 
 export default NotificationDateTimeSetting;
